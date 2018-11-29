de07b334b267f7830d74b05fa7e474b4a9095b6c
sengedateformat <605480673@qq.com>
2018-11-29 09:38:53 +0800

    new Java Class

---
+++ src/main/java/com/aaa/testgh/controller/EmpController.java
@@ -0,0 +1,11 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:EmpController
+ * discription:
+ * author:ZhangSenYao
+ * createTime:2018-11-29 09:38
+ */
+public class EmpController {
+
+}
